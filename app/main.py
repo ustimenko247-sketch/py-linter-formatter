194f11ce57991326b804332b3e994e5291e454f9
Your Name <denys@mate.academy>
2026-04-27 13:00:24 +0300

    Fix linter errors and tests

--- app/main.py
+++ app/main.py
@@ -16,7 +16,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     }
 
 
-
 def format_linter_report(errors_by_file: dict) -> list:
     return [
         format_single_linter_file(path, errors)
